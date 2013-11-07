0466d160a8272d954205910f269da813c184e14b
Aleksey Gureiev <spyromus@noizeramp.com>
2013-11-07 20:21:06 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Enrs::Application.config.secret_key_base = '9326b15c187f41839280fe72320199c8c311f42f2056ad6a049f39ecf67f02ecab025480972d0d0067a8d135409522641ed31378275e6467b694f5478d96d0f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Enrs::Application.config.session_store :cookie_store, key: '_enrs_session'
