d09fd17324c7a4fc412e71cf77f20627e4b38275
Aleksey Gureiev <spyromus@noizeramp.com>
2013-12-05 08:07:44 +0200

    disabled entities compilation

--- app/assets/javascripts/results.js
+++ app/assets/javascripts/results.js
@@ -3,7 +3,7 @@
 //= require backbone.marionette
 //= require_tree ./backbone/config
 //= require backbone/app
-//= require_tree ./backbone/entities
+// require_tree ./backbone/entities
 //= require_tree ./backbone/apps
 
 ENRS.start();
