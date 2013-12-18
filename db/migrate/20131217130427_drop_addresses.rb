6fcca7a65230626aaf4723b8e55f18185118df03
Aleksey Gureiev <spyromus@noizeramp.com>
2013-12-19 00:27:31 +0200

    fixed addresses migration issue

--- db/migrate/20131217130427_drop_addresses.rb
+++ db/migrate/20131217130427_drop_addresses.rb
@@ -1,17 +1,9 @@
 class DropAddresses < ActiveRecord::Migration
   def up
-    remove_column :polling_locations, :address_id
-    drop_table :addresses
+    #remove_column :polling_locations, :address_id
   end
 
   def down
-    create_table :addresses, force: true do |t|
-      t.string "line1"
-      t.string "line2"
-      t.string "city"
-      t.string "state"
-      t.string "zip"
-    end
     add_column :polling_locations, :address_id
     add_index  :polling_locations, :address_id, name: "index_polling_locations_on_address_id", using: :btree
   end
