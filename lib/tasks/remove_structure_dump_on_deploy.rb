bcc85448bde3097d2a70498094e7419abf9d4172
Aleksey Gureiev <spyromus@noizeramp.com>
2014-04-30 15:23:03 +0400

    attempt to remove structure dump on deploy

---
+++ lib/tasks/remove_structure_dump_on_deploy.rb
@@ -0,0 +1,17 @@
+env = ENV['RACK_ENV']
+if env == 'production' || env == 'staging'
+  Rake::TaskManager.class_eval do
+    def remove_task(task_name)
+      @tasks.delete(task_name.to_s)
+    end
+  end
+
+  Rake.application.remove_task('db:structure:dump')
+  namespace :db do
+    namespace :structure do
+      task :dump do
+        # Overridden to do nothing
+      end
+    end
+  end
+end
