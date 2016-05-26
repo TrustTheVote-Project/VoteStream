b7a759fec233751c9526dd44df7932943db4b68a
Alex Mekelburg <alex@mekware.com>
2016-05-26 16:11:19 -0400

    floating math

--- app/models/ref_con_results.rb
+++ app/models/ref_con_results.rb
@@ -573,7 +573,8 @@ class RefConResults
     sorted_parties = hash_of_parties.to_a.sort {|ar1, ar2| ar2[1]<=>ar1[1]}
     puts sorted_parties
     color = sorted_parties[0][0][0].downcase # just take the first letter
-    diff = ((sorted_parties[0][1] - sorted_parties[1][1]) / sorted_parties[0][1]) * 100
+    diff = ((sorted_parties[0][1] - sorted_parties[1][1]) / sorted_parties[0][1].to_f) * 100
+    raise diff.to_s
     if diff < AppConfig['map_color']['threshold']['lower']
       shade = 2
     elsif diff < AppConfig['map_color']['threshold']['upper']
