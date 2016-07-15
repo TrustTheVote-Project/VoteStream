51038caef693bb7cfe619af3a8769012ea7e76ed
Alex Mekelburg <alex@mekware.com>
2016-07-15 12:46:19 -0400

    dont have asset version in production.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,6 +33,12 @@ namespace :deploy do
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
+      # within release_path do
+      #   execute :rake, 'assets:precompile'
+      # end
+      # within release_path do
+      #   execute :rake, 'tmp:cache:clear'
+      # end
     end
   end
 
